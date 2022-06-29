ab7736e9e6c9f80c4d86ebc623ecf7cc9d6b0e6d
Swarnadeep <swarnadeeppramanik@gmail.com>
2022-06-29 20:50:53 +0530

    added space

--- src/main/java/com/quickLaunch/CorsConfig.java
+++ src/main/java/com/quickLaunch/CorsConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 	
+	
+	
+	
 		@Bean
 		public WebMvcConfigurer corsConfigurer() {
 			return new WebMvcConfigurer() {
